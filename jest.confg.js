a9549d1412260dc891a80aa99f9763cb184b09d3
Joberth Rogers <joberth.rogers18@gmail.com>
2024-05-23 09:56:18 -0300

    config: add configuration jest to list coverage

--- jest.confg.js
+++ jest.confg.js
@@ -2,4 +2,12 @@ module.exports = {
   testEnvironment: "node",
   moduleFileExtensions: ["js", "json", "node"],
   transformIgnorePatterns: ["/node_modules/"],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx,ts,tsx}",
+    "!src/**/*.test.{js,jsx,ts,tsx}",
+    "!src/**/index.{js,jsx,ts,tsx}",
+  ],
+  coverageDirectory: "coverage",
+  coverageReporters: ["json", "lcov", "text", "clover"],
 };
