adc9ff31331386579a4e85f7b56b5ea784644c5d
Joberth Rogers <joberth.rogers18@gmail.com>
2024-05-24 17:35:49 -0300

    feat: load task successfully in frontend

--- frontend/src/pages/Todo/index.js
+++ frontend/src/pages/Todo/index.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Button } from "primereact/button";
+import { Toast } from "primereact/toast";
 
 import AlertInfo from "../../components/AlertInfo";
 import ListTask from "../../components/ListTask";
@@ -8,6 +9,27 @@ import CreationTask from "../../components/CreationTask";
 
 function Todo() {
   const [visible, setVisible] = useState(false);
+  const [tasks, setTasks] = useState([]);
+  const toast = useRef(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/tarefa");
+        const result = await response.json();
+        setTasks(result.data);
+      } catch (error) {
+        toast.current.show({
+          severity: "error",
+          summary: "Erro ao carregar dados",
+          detail:
+            "Não possível carregar os dados das tarefas. Tente novamente mais tarde!",
+        });
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div className="todo-wrapper w-full flex justify-content-center align-items-center p-3">
@@ -23,6 +45,7 @@ function Todo() {
         <ListTask />
       </div>
       <CreationTask visible={visible} onChangeVisible={setVisible} />
+      <Toast ref={toast} />
     </div>
   );
 }
