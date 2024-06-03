6a18b28720e870cd7288f5d0171da1d558734d77
Joberth Rogers <joberth.rogers18@gmail.com>
2024-06-03 00:23:45 -0300

    Update envVariables.js
    fix: change the scope api url

--- frontend/src/constants/envVariables.js
+++ frontend/src/constants/envVariables.js
@@ -1,4 +1,4 @@
 export const envVariables = {
-  // BASE_URL: process.env.API_URL || "http://44.220.139.209:3000",
-  BASE_URL: process.env.API_URL || "http://localhost:3000",
+  BASE_URL: process.env.API_URL || "http://44.220.139.209:3000",
+  // BASE_URL: process.env.API_URL || "http://localhost:3000",
 };
